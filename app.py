7b5a70f211b5c26ffe33cd739a75e2a8e8583c08
Logan Zechella <jloganzechella@gmail.com>
2023-12-13 22:06:13 -0500

    Back end changes

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import os
 from serpapi import GoogleSearch
 
-load_dotenv()
+if os.environ.get("FLASK_ENV") == "development":
+    load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Allow CORS so that frontend can access backend served from different domains.
@@ -105,4 +106,4 @@ def stock_data(company_name):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
